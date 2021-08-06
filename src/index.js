cb0a9a65bb6896b1bea8a1a7da2ca7059d4a4e27
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-08-06 19:39:30 -0400

    making it default

--- src/index.js
+++ src/index.js
@@ -3,60 +3,6 @@ import ReactDOM from "react-dom";
 
 import "index.scss";
 
-// import Application from "components/Application";
+import Application from "components/Application";
 
-import tweets from './tweets.json'
-
-
-
-// ReactDOM.render(<Application />, document.getElementById("root"));
-
-import "./styles.css";
-
-// const tweet = tweets[0];
-
-function Tweet(props) {
-  return (
-    <article className="tweet">
-      <header className="tweet__header">
-        <img className="tweet__header-avatar" src={ props.avatar } alt="avatar" />
-        <h2 className="tweet__header-name">{ props.name }</h2>
-      </header>
-      <main className="tweet__content">
-        <p>{ props.content }</p>
-      </main>
-      <footer className="tweet__footer">{ props.date }</footer>
-    </article>
-  );
-}
-
-// ReactDOM.render(
-//   <Tweet
-//     name={tweet.name}
-//     avatar={tweet.avatar}
-//     content={tweet.content}
-//     date={tweet.date}
-//   />,
-//   document.getElementById("root")
-// );
-
-function TweetList(props) {
-  const tweets = props.tweets.map(tweet => {
-    return (
-      <Tweet
-        key={tweet.id}
-        name={tweet.name}
-        avatar={tweet.avatar}
-        content={tweet.content}
-        date={tweet.date}
-      />
-    );
-  });
-
-  return tweets;
-}
-
-ReactDOM.render(
-  <TweetList tweets={tweets} />,
-  document.getElementById("root")
-);
+ReactDOM.render(<Application />, document.getElementById("root"));
