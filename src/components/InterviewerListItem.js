945eb61d979be9f1d55d069909af6a82aaf551d4
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-08-06 20:07:50 -0400

    InterviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "components/InterviewerListItem.scss";
+
+import classnames from "classnames";
+
+export default function DayListItem(props) {
+
+  const liClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
+
+  return (
+    <li className={liClass} onClick={() => props.setInterviewer(props.name)}>
+      <img
+        key={props.id}
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+  );
+}
