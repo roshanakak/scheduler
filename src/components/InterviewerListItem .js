d341daada575fdcfc8616afe9f8cd9ac8ee77311
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-08-06 20:00:06 -0400

    InterviewerListItem

---
+++ src/components/InterviewerListItem .js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "components/InterviewerListItem .scss";
+
+import classnames from "classnames";
+
+export default function DayListItem(props) {
+  
+
+  return (
+    <li className="interviewers__item">
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      Sylvia Palmer
+    </li>
+  );
+}
