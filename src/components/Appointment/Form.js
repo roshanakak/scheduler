ef0454f9d011ebbf2352987ae8cad66c0834dd03
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-08-07 13:07:24 -0400

    Form/states done

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,52 +1,69 @@
-import React, { useState } from 'react'
-
-import "./styles.scss";
-import Button from "../Button";
-import InterviewerList from "../InterviewerList";
+import React, { useState } from 'react';
+import Button from '../Button';
+import InterviewerList from '../InterviewerList';
 
 export default function Form(props) {
-  const [name, setName] = useState(props.name || "");
+  const { interviewers, onCancel, onSave } = props;
+
+  const [name, setName] = useState(props.name || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
-  
-  const reset = function() {
-    setName("");
-    setInterviewer("");
+  function reset() {
+    setName('');
+    setInterviewer(null);
+    setError("");
   }
-  
-  const cancel = function() {
+
+  function cancel() {
     reset();
-    props.onCancel();
+    onCancel();
   }
 
-
-
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    setError("");
+    onSave(name, interviewer);
+  }
+  
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
+            value={name}
             placeholder="Enter Student Name"
-            onChange={(event) => setName(event.target.value)}
-            
+            onChange={event => setName(event.target.value)}
+            data-testid="student-name-input"
           />
-          <h1>Hello, {name}.</h1>
+          <section className="appointment__validation">{error}</section>
         </form>
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={(event) => setInterviewer(event.target.value)}
+        <InterviewerList
+          interviewers={interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
         />
-        <h1>Hello, {interviewer}.</h1>
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button
+            confirm
+            onClick={validate}
+          >
+            Save
+          </Button>
         </section>
       </section>
     </main>
-  )
-
-
+  );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from 'react'
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -146,7 +146,12 @@ storiesOf("Button", module)
         .add("Appointment", () => <Appointment />)
         .add("Appointment with Time", () => <Appointment time="12pm" />)
         .add("Header", () => <Header time="12pm" />)
-        .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+        .add("Appointment Empty", () => (
+          <Fragment>
+            <Appointment id={1} time="12pm" />
+            <Appointment id="last" time="1pm" />
+          </Fragment>
+        ))
         .add("Show", () => (
           <Show
             student="Lydia Miller-Jones"
